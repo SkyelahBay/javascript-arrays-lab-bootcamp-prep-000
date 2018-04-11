3339159227bebc96729b8bc01917522b1887fba3
Skyelah Hanna Bay <skyelahbay@gmail.com>
2018-04-11 02:10:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,32 @@
-const app = "I don't do much."
-function destructivelyAppendKitten(){
-  
+const app = "I don't do much.";
+
+function destructivelyAppendKitten(arrayName, elementToAdd){
+  arrayName.push(elementToAdd);
+  return arrayName;
+}
+function destructivelyPrependKitten(arrayName, elementToAdd){
+  arrayName.unshift(elementToAdd);
+  return arrayName;
+}
+function destructivelyRemoveLastKitten(arrayName){
+  arrayName.pop();
+  return arrayName;
+}
+function destructivelyRemoveFirstKitten(arrayName){
+  arrayName.shift();
+  return arrayName;
+}
+function appendKitten(arrayName, elementToAdd){
+  return [...arrayName, elementToAdd];
+}
+function prependKitten(arrayName, elementToAdd){
+  return [elementToAdd, ...arrayName];
+}
+function removeLastKitten(arrayName){
+  let newArray = arrayName.slice(0, arrayName.length);
+  return newArray;
+}
+function removeFirstKitten(arrayName){
+  let newArray = arrayName.slice(1);
+  return newArray;
 }
